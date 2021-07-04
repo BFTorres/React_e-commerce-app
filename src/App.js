5ba6e06e700dc6ff3b3fe93832fe40aa3bcb2b49
BFTorres <bfonseca@gmx.de>
2021-07-05 00:03:36 +0200

    products shipping commercejs init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
 
 // rafce
 const App = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    const fetchcart = async () => {
+        setCart(await commmerce.cart.retrieve());
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const response = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
     return (
         <div>
             <Navbar />
-            <Products />
+            <Products products={products} onAddToCart={handleAddToCart}/>
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'E-Commerce S', description: 'Online Shop for smal businesses', price: '2500,- €', image: 'https://images.unsplash.com/photo-1556742502-ec7c0e9f34b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'},
+/* const products = [
+    { id: 1, name: 'E-Commerce S', description: 'Online Shop for small businesses', price: '2500,- €', image: 'https://images.unsplash.com/photo-1556742502-ec7c0e9f34b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'},
     { id: 2, name: 'Portfolio S', description: 'Static Portfolio with Responsive Design', price: '1200,- €', image: 'https://images.unsplash.com/photo-1527239441953-caffd968d952?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'}, 
 
-];
+]; */
 
-const Products = () => {
+const Products = ({ products,onAddToCart }) => {
+    const classes = useStyles();
     return (
         <main>
             <Grid container justify="center" spacing={4}>
@@ -19,7 +21,7 @@ const Products = () => {
                 ))}
             </Grid>
         </main>
-    )
-}
+    );
+};
 
 export default Products;
