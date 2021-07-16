205ef94684925d560b3149eaff400f29c4a96787
BFTorres <bfonseca@gmx.de>
2021-07-16 16:33:38 +0200

    base functionalities and darkmode preference working

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
 import Product from './Product/Product';
 import useStyles from './styles';
 
-/* const products = [
-    { id: 1, name: 'E-Commerce S', description: 'Online Shop for small businesses', price: '2500,- €', image: 'https://images.unsplash.com/photo-1556742502-ec7c0e9f34b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'},
-    { id: 2, name: 'Portfolio S', description: 'Static Portfolio with Responsive Design', price: '1200,- €', image: 'https://images.unsplash.com/photo-1527239441953-caffd968d952?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'}, 
-
-]; */
-
 const Products = ({ products, onAddToCart }) => {
-    const classes = useStyles();
-    return (
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} onAddToCart={onAddToCart} />
-                    </Grid>
-                ))}
-            </Grid>
-        </main>
-    );
+  const classes = useStyles();
+
+  if (!products.length) return <p>Loading...</p>;
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 };
 
 export default Products;
+
